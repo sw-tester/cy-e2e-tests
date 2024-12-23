dc31cbdc51613df1c2a7a61e4b9826a2549d6493
Veera <nmani0303@gmail.com>
2024-12-23 13:28:52 +0530

    init commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions:{
+    overwrite: false
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://text2data.com'
   },
